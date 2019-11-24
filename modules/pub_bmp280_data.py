3946161ae97272232f58f0f980290b3a346bda80
Alan Cudmore <alan.cudmore@gmail.com>
2019-11-23 22:07:13 -0500

    updated cvt, data screens, pub script

--- modules/pub_bmp280_data.py
+++ modules/pub_bmp280_data.py
@@ -1,25 +1,37 @@
+#!/usr/bin/env python3
+
+#
+# Publish LSM9DS1 data to MQTT
+# This will publish the acceleration, magnetometer,
+# and gyroscope values every second.
+#
+import paho.mqtt.client as mqtt
 import time
-
 import board
-# import digitalio # For use with SPI
 import busio
-
 import adafruit_bmp280
+
+broker_address = "127.0.0.1"
+client = mqtt.Client("BMP280")
+client.connect(broker_address)
 
 # Create library object using our Bus I2C port
 i2c = busio.I2C(board.SCL, board.SDA)
 bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
 
-# OR create library object using our Bus SPI port
-#spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-#bmp_cs = digitalio.DigitalInOut(board.D10)
-#bmp280 = adafruit_bmp280.Adafruit_BMP280_SPI(spi, bmp_cs)
-
 # change this to match the location's pressure (hPa) at sea level
 bmp280.sea_level_pressure = 1013.25
 
 while True:
-    print("\nTemperature: %0.1f C" % bmp280.temperature)
-    print("Pressure: %0.1f hPa" % bmp280.pressure)
-    print("Altitude = %0.2f meters" % bmp280.altitude)
+    temp = bmp280.temperature
+    press = bmp280.pressure
+    alt = bmp280.altitude
+    # print("\nTemperature: %0.1f C" % temp)
+    # print("Pressure: %0.1f hPa" % press)
+    # print("Altitude = %0.2f meters" % alt)
+
+    client.publish('psz/bmp280/temp', '{0:0.1f}'.format(temp))
+    client.publish('psz/bmp280/pressure', '{0:0.1f}'.format(press))
+    client.publish('psz/bmp280/altitude', '{0:0.2f}'.format(alt))
+ 
     time.sleep(2)
