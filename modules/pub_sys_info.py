4c5cb6859fe6a7765d984510d4461703e6ba1da8
Alan Cudmore <alan.cudmore@gmail.com>
2019-11-24 22:11:01 -0500

    working on completing sys info

--- modules/pub_sys_info.py
+++ modules/pub_sys_info.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+# Publish the following info to MQTT
+# "psz/sys/ipaddr"   
+# "psz/sys/hostname"
+# "psz/sys/cpuutil" 
+# "psz/sys/memfree" 
+# "psz/sys/diskfree"
+
 #
 # Publish System data to MQTT
 #
@@ -9,6 +16,7 @@ import sys
 import socket
 import subprocess
 import paho.mqtt.client as mqtt
+import psutil
 
 mqtt_broker_address = '127.0.0.1'
 client = mqtt.Client("SYSINFO")
@@ -30,17 +38,20 @@ hostname = socket.gethostname()
 
 cpuutil = '10.0'
 memfree =  '256'
+diskfree = '1234'
 
 while True:
     # print ("IP Address = ", ip_address.decode())
     # print ("Hostname = ",hostname)
     # print ("Cpu util = ",cpuutil)
     # print ("Mem = ",memfree)
+    # print ("disk = ",diskfree)
 
     client.publish('psz/sys/ipaddr',   ip_address)
     client.publish('psz/sys/hostname', hostname)
-    client.publish('psz/sys/cpuutil', cpuutil)
-    client.publish('psz/sys/memfree', memfree)
+    client.publish('psz/sys/cpuutil',  cpuutil)
+    client.publish('psz/sys/memfree',  memfree)
+    client.publish('psz/sys/diskfree', diskfree)
 
-    time.sleep(2)
+    time.sleep(5)
 
