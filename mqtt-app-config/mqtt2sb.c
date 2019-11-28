ee59c0441169465bf0796acec5de466dffac390f
Alan Cudmore <Alan.Cudmore@gmail.com>
2019-11-27 23:32:41 -0500

    added cFS MQTT app configuration files

---
+++ mqtt-app-config/mqtt2sb.c
@@ -0,0 +1,476 @@
+/*************************************************************************
+** File:
+**   $Id: mqtt2sb_table.c $
+**
+** Copyright 2018-2019 - Alan Cudmore
+**
+** Purpose: 
+**   MQTT to Software Bus routing table 
+**
+** Notes:
+** 
+** 
+*************************************************************************/
+
+/*************************************************************************
+** Includes
+*************************************************************************/
+#include "mqtt_app.h"
+#include "cfe_tbl_filedef.h"
+
+/*************************************************************************
+** Exported Data
+*************************************************************************/
+/*
+** Table file header
+*/
+static CFE_TBL_FileDef_t CFE_TBL_FileDef __attribute__((__used__)) =
+{
+    "MQTT_Mqtt2SbTable", MQTT_APP_NAME "." MQTT_MQTT2SB_TABLENAME,
+    "MQTT MQTT to SB table", "mqtt2sb.tbl",
+    (sizeof(MQTT_Mqtt2SbTableEntry_t) * MQTT_MQTT2SB_MAX_TBL_ENTRIES)
+};
+
+/*
+** This is the MQTT to Software Bus table (mqtt2sb) 
+** 
+** Each table entry has the following:
+**  Type: MQTT_UNUSED, MQTT_TLM_PKT, MQTT_CMD_PACKET
+**  Message ID:
+**  Data Type:
+**  Data Length: (may not be needed)
+*/
+MQTT_Mqtt2SbTableEntry_t MQTT_Mqtt2SbTable[MQTT_MQTT2SB_MAX_TBL_ENTRIES] =
+{
+    /*
+    ** ProtoSat Zero System Information 
+    */
+    {
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_SYS_IPADDR_TLM_MID,
+        .MqttTopic         = "psz/sys/ipaddr",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_STRING16,
+        .DataLength        = 16
+    },
+    {
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_SYS_HOSTNAME_TLM_MID,
+        .MqttTopic          = "psz/sys/hostname",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_STRING32,
+        .DataLength        = 32
+    },
+    {
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_SYS_CPUUTIL_TLM_MID
+        .MqttTopic          = "psz/sys/cpuutil",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_FLOAT,
+        .DataLength        = 4
+    },
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_SYS_MEMFREE_TLM_MID
+        .MqttTopic          = "psz/sys/memfree",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_FLOAT,
+        .DataLength        = 4
+    },
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_SYS_DISKFREE_TLM_MID
+        .MqttTopic          = "psz/sys/diskfree",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_FLOAT,
+        .DataLength        = 4
+    },
+
+    /*
+    ** ProtoSatZero cFS Status 
+    */
+
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_CFS_STATUS_TLM_MID,
+        .MqttTopic          = "psz/cfs/status",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_STRING16,
+        .DataLength        = 16
+    },
+
+    /*
+    ** ProtoSatZero BMP280 
+    */
+
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_BMP280_TEMP_TLM_MID,
+        .MqttTopic          = "psz/bmp280/temp",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_FLOAT,
+        .DataLength        = 4
+    },
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_BMP280_PRESSURE_TLM_MID,
+        .MqttTopic          = "/psz/bmp280/pressure",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_FLOAT,
+        .DataLength        = 4
+    },
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_BMP280_ALTITUDE_TLM_MID,
+        .MqttTopic          = "psz/bmp280/altitude",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_FLOAT,
+        .DataLength        = 4
+    },
+
+    /*
+    ** ProtoSatZero LSM9DS1 
+    */
+
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_LSM9DS1_GYRO_X_TLM_MID,
+        .MqttTopic         = "psz/lsm9ds1/gyro_x",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_FLOAT,
+        .DataLength        = 4
+    },
+    {
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_LSM9DS1_GYRO_Y_TLM_MID,
+        .MqttTopic         = "psz/lsm9ds1/gyro_y",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_FLOAT,
+        .DataLength        = 4
+    },
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_LSM9DS1_GYRO_Z_TLM_MID,
+        .MqttTopic         = "psz/lsm9ds1/gyro_z",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_FLOAT,
+        .DataLength        = 4
+    },
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_LSM9DS1_MAG_X_TLM_MID,
+        .MqttTopic         = "psz/lsm9ds1/mag_x",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_FLOAT,
+        .DataLength        = 4
+    },
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_LSM9DS1_MAG_Y_TLM_MID,
+        .MqttTopic         = "psz/lsm9ds1/mag_y",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_FLOAT,
+        .DataLength        = 4
+    },
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_LSM9DS1_MAG_Z_TLM_MID,
+        .MqttTopic         = "psz/lsm9ds1/mag_z",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_FLOAT,
+        .DataLength        = 4
+    },
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_LSM9DS1_ACCEL_X_TLM_MID,
+        .MqttTopic         = "psz/lsm9ds1/accel_x",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_FLOAT,
+        .DataLength        = 4
+    },
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_LSM9DS1_ACCEL_Y_TLM_MID,
+        .MqttTopic         = "psz/lsm9ds1/accel_y",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_FLOAT,
+        .DataLength        = 4
+    },
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_LSM9DS1_ACCEL_Z_TLM_MID,
+        .MqttTopic         = "psz/lsm9ds1/accel_z",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_FLOAT,
+        .DataLength        = 4
+    },
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_LSM9DS1_TEMP_TLM_MID,
+        .MqttTopic         = "psz/lsm9ds1/temp",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_FLOAT,
+        .DataLength        = 4
+    },
+
+    /*
+    ** ProtoSatZero APDS9960 Color/Gesture/Prox sensor 
+    */
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_APDS9960_RED_TLM_MID,
+        .MqttTopic         = "psz/apds9960/red",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+    {
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_APDS9960_GREEN_TLM_MID,
+        .MqttTopic         = "psz/apds9960/green",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_APDS9960_BLUE_TLM_MID,
+        .MqttTopic         = "psz/apds9960/blue",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_APDS9960_CLEAR_TLM_MID,
+        .MqttTopic         = "psz/apds9960/clear",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_APDS9960_COLOR_TMP_TLM_MID,
+        .MqttTopic         = "psz/apds9960/color_temp",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_FLOAT,
+        .DataLength        = 4
+    },
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_APDS9960_LUX_TLM_MID,
+        .MqttTopic         = "psz/apds9960/lux",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_FLOAT,
+        .DataLength        = 4
+    },
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_APDS9960_PROX_TLM_MID,
+        .MqttTopic         = "psz/apds9960/prox",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_FLOAT,
+        .DataLength        = 4
+    },
+    {   
+        .MsgType           = MQTT_PRIMITIVE,
+        .MsgId             = MQTT_APDS9960_GESTURE_TLM_MID,
+        .MqttTopic         = "psz/apds9960/gesture",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_STRING16,
+        .DataLength        = 4 
+    },
+
+    /* ------------------ Unused ----------------------- */
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    /* 30 */
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    /* 40 */
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    },
+
+    {   .MsgType           = MQTT_UNUSED,
+        .MsgId             = 0,
+        .MqttTopic          = "null",
+        .PktType           = MQTT_TLM_PKT,
+        .DataType          = MQTT_INT32,
+        .DataLength        = 4
+    }
+}; 
+
+/************************/
+/*  End of File Comment */
+/************************/
---
+++ mqtt-app-config/mqtt_msgids.h
@@ -0,0 +1,86 @@
+/*=======================================================================================
+** File Name:  mqtt_msgids.h
+**
+** Title:  Message ID Header File for MQTT Application
+**
+** $Author:    Alan Cudmore
+** $Revision: 1.1 $
+** $Date:      2018-07-21
+**
+** Purpose:  This header file contains declartions and definitions of all MQTT's 
+**           Message IDs.
+**
+** Modification History:
+**   Date | Author | Description
+**   ---------------------------
+**   2018-07-21 | Alan Cudmore | Build #: Code Started
+**
+**=====================================================================================*/
+#ifndef _MQTT_MSGIDS_H_
+#define _MQTT_MSGIDS_H_
+
+#define MQTT_CMD_MID            	0x18F0
+#define MQTT_SEND_HK_MID        	0x18F1
+
+#define MQTT_HK_TLM_MID		        0x08F0
+
+
+/* Until I have a better idea, need to define the message IDs of packets that will be sent out */
+
+#define MQTT_CMD_1_MID                0x1900
+
+/*
+** Linux System messages
+*/
+#define MQTT_SYS_IPADDR_MID           0x0900
+#define MQTT_SYS_HOSTNAME_MID         0x0901
+#define MQTT_SYS_CPUUTIL_TLM_MID      0x0902
+#define MQTT_SYS_MEMFREE_TLM_MID      0x0903
+#define MQTT_SYS_DISKFREE_TLM_MID     0x0904
+
+/*
+** cFS System messages
+*/
+#define MQTT_CFS_STATUS_TLM_MID       0x0905
+#define MQTT_CFS_2_TLM_MID            0x0906
+#define MQTT_CFS_3_TLM_MID            0x0907
+
+/*
+** BMP280 messages
+*/
+#define MQTT_BMP280_TEMP_TLM_MID      0x090A
+#define MQTT_BMP280_PRESSURE_TLM_MID  0x090B
+#define MQTT_BMP280_ALTITUDE_TLM_MID  0x090C
+
+/*
+** LSM9DS1 messages
+*/
+#define MQTT_LSM9DS1_GYRO_X_TLM_MID    0x0910
+#define MQTT_LSM9DS1_GYRO_Y_TLM_MID    0x0911
+#define MQTT_LSM9DS1_GYRO_Z_TLM_MID    0x0912
+#define MQTT_LSM9DS1_MAG_X_TLM_MID     0x0913
+#define MQTT_LSM9DS1_MAG_Y_TLM_MID     0x0914
+#define MQTT_LSM9DS1_MAG_Z_TLM_MID     0x0915
+#define MQTT_LSM9DS1_ACCEL_X_TLM_MID   0x0916
+#define MQTT_LSM9DS1_ACCEL_Y_TLM_MID   0x0917
+#define MQTT_LSM9DS1_ACCEL_Z_TLM_MID   0x0918
+#define MQTT_LSM9DS1_TEMP_TLM_MID      0x0919
+
+/*
+** APDS9960 Messages
+*/
+#define MQTT_APDS9960_RED_TLM_MID       0x0950
+#define MQTT_APDS9960_BLUE_TLM_MID      0x0951
+#define MQTT_APDS9960_GREEN_TLM_MID     0x0952
+#define MQTT_APDS9960_CLEAR_TLM_MID     0x0953
+#define MQTT_APDS9960_COLOR_TMP_TLM_MID 0x0954
+#define MQTT_APDS9960_LUX_TLM_MID       0x0955
+#define MQTT_APDS9960_PROX_TLM_MID      0x0956
+#define MQTT_APDS9960_GESTURE_TLM_MID   0x0957
+
+#endif /* _MQTT_MSGIDS_H_ */
+
+/*=======================================================================================
+** End of file mqtt_msgids.h
+**=====================================================================================*/
+    
