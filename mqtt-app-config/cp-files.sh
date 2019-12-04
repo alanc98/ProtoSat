c60a39dde58d78679c2bbb150c67c73e7814421c
Alan Cudmore <alan.cudmore@gmail.com>
2019-12-03 23:02:30 -0500

    added script to copy changed cFS files

---
+++ mqtt-app-config/cp-files.sh
@@ -0,0 +1,5 @@
+cp mqtt_msgids.h /home/pi/cFS/apps/mqtt/fsw/platform_inc/mqtt_msgids.h
+cp mqtt_child.c  /home/pi/cFS/apps/mqtt/fsw/src/mqtt_child.c
+cp mqtt2sb.c	/home/pi/cFS/apps/mqtt/fsw/tables/mqtt2sb.c
+cp to_lab_sub_table.h /home/pi/cFS/apps/to_lab/fsw/platform_inc/to_lab_sub_table.h
+
